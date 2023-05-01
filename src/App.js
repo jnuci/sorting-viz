38837755cf47673eaba0f44383fec00b5ffed141
Jaime Nuci <jnuci@ucsb.edu>
2023-04-30 18:59:30 -0700

    Update site icon and title.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import SortingVisualizer from './SortingVisualizer/SortingVisualizer';
 import './App.css';
+import { Helmet } from "react-helmet";
 
 
 function App() {
   return (
     <div className="App">
+      <Helmet>
+        <meta charSet='utf-8' />
+        <title>Sorting Bars</title>
+        <meta name="description" content = "Sorting Algorithm Visualizer" />
+      </Helmet>
       <SortingVisualizer></SortingVisualizer>
     </div>
   );
